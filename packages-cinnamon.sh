220eb62e2e517e6f1d3f394fe2050b29fce2729d
matzzze <83610709+matzzze@users.noreply.github.com>
2021-05-03 22:11:58 +0200

    Create packages-cinnamon.sh

---
+++ packages-cinnamon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+    then echo "must run as root"
+        exit
+fi
+
+echo "=====> downloading official packages"
+echo
+# requires sudo
+# --noconfirm is used to select all packages from groups
+pacman -Sy --needed $(<packages-cinnamon.txt)
